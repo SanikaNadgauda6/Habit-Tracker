6e25c4eb096c0c94a6625d8a650f6f1ff9bcd3f1
SanikaNadgauda6 <134848204+SanikaNadgauda6@users.noreply.github.com>
2023-06-25 20:08:55 +0530

    Add files via upload

--- Alarm Clock/alarm.js
+++ Alarm Clock/alarm.js
@@ -1,18 +1,12 @@
-// Get the submit button element
 var setAlarm = document.getElementById('submit');
-
-// Add an event listener to the submit button
 setAlarm.addEventListener('click', setNewAlarm);
 
 // Get the alarms list container element
 const alarmsList = document.getElementById("alarmsList");
-
-// Array to store alarms
 const alarmsArray = [];
 
 // Function to create and display an alarm in the list
 function displayAlarms() {
-  // Clear the alarms list
   alarmsList.innerHTML = '';
 
   // Iterate over the alarmsArray and display each alarm
@@ -20,23 +14,8 @@ function displayAlarms() {
     // Create a new list item element
     const listItem = document.createElement("li");
 
-//   // Convert the hours to 12-hour format
-//   let hour12 = alarm.hour % 12;
-//   hour12 = hour12 === 0 ? 12 : hour12;
-
-//   // Determine if it's AM or PM
-//   const period = alarm.hour >= 12 ? 'PM' : 'AM';
-
-//   // Set the text content of the list item with the formatted time
-//   listItem.textContent = `${hour12}:${formattedMinute} ${period}`;
-
-
-//   // Format the hours and minutes with leading zeros
-//   const formattedHour = alarm.hour.toString().padStart(2, '0');
-//   const formattedMinute = alarm.minute.toString().padStart(2, '0');
-
     // Set the text content of the list item
-    listItem.textContent = `${alarm.hour}:${alarm.minute}`;
+    listItem.textContent = `${alarm.hour}:${alarm.minute}  ${alarm.AmPm}`;
 
     // Create a delete button for each alarm
     const deleteButton = document.createElement("button");
@@ -54,39 +33,36 @@ function displayAlarms() {
   });
 }
 
-
 function setNewAlarm() {
   // Get the date input element
-  var alarmDate = document.getElementById('date');
-  alarmDate = new Date(alarmDate.value);
-  currentDate = new Date().getTime();
+  var InputalarmDate = document.getElementById('date');
+  alarmDate = new Date(InputalarmDate.value);
+  let currentDate = new Date().getTime();
+  let timeToAlarm = alarmDate - currentDate;
 
   // Ring the bell at the time of alarm
-  var timeToAlarm = alarmDate - currentDate;
   var tune = new Audio("alarm-clock-tune.mp3");
   if (timeToAlarm >= 0) {
     setTimeout(() => {
       tune.play();
+      // Remove the alarm from the array after it has rung
+      alarmsArray.shift();
+      displayAlarms();
     }, timeToAlarm);
   }
 
   // Add the new alarm to the array
-  const alarmHour = alarmDate.getHours(); // Get the hours (0-23)
-  if (alarmHour.length < 2) {
-    alarmHour = '0' + alarmHour;
-  }
-  const alarmMinute = alarmDate.getMinutes(); // Get the minutes (0-59)
-
-  if (alarmMinute.length < 2) {
-    alarmMinute = '0' + alarmMinute;
-  }
-
+  let alarmHour = alarmDate.getHours().toString().padStart(2, '0'); // Get the hours (0-23)
+  var AmPm = alarmHour >= 12 ? 'PM' : 'AM';
+  alarmHour = (alarmHour % 12) || 12;
+  const paddedAlarmHour = alarmHour.toString().padStart(2, '0');
+  const alarmMinute = alarmDate.getMinutes().toString().padStart(2, '0'); // Get the minutes (0-59)
 
   const alarm = {
-    hour: alarmHour,
+    hour: paddedAlarmHour,
     minute: alarmMinute,
+    AmPm: AmPm,
   };
-  
 
   alarmsArray.push(alarm); // Add the new alarm to the array
   displayAlarms(); // Display all alarms in the list
@@ -106,25 +82,3 @@ setInterval(() => {
   let liveDate = new Date();
   document.getElementById("currentTime").innerHTML = liveDate.toLocaleTimeString();
 }, 1000);
-
-
-
-
-
-
-
-//    // counting how many days , hrs, mins & secs are remaining for the alarm
-//         // var days = Math.floor( timeToAlarm / (1000*60*60*24));
-
-//         // var hours = Math.floor( (timeToAlarm % (1000*60*60*24)) / (1000*60*60) );
-//         // // console.log(hours, "Hours");
-
-//         // var minutes = Math.floor((timeToAlarm % (1000*60*60)) / (1000*60) );
-//         // // console.log(minutes, "Mins");
-
-//         // var seconds = Math.floor((timeToAlarm % (1000*60)) / 1000 );
-//         // // console.log(seconds, "secs");
-
-//         // var Show = days +" Day(s), "+hours+" hrs, "+ minutes+" min, "+seconds+" secs. ";
-
-//         // document.getElementById("show").innerHTML = Show;
